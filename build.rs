b4a24467433c80107429f848a914096258886917
shaunluedeke <shaun.luedeke5520@gmail.com>
2026-01-10 01:29:17 +0100

    Rename project to **ShellyRGBAudio**, update metadata in `Cargo.toml`, add `build.rs` for Windows resource compilation, and include project icon.

---
+++ build.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/ShellyRGBAudio.ico");
+    res.set("FileDescription", "Music responsive Shelly LED controller");
+    res.set("ProductName", "ShellyRGBAudio");
+    res.set("CompanyName", "SytxLabs");
+    res.set("LegalCopyright", "© 2026 SytxLabs");
+    res.set("OriginalFilename", "ShellyRGBAudio.exe");
+    res.compile().unwrap();
+}
